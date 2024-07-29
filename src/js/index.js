882139a2c1fe713d0e6a4fd5dd930a55e2e02226
megharaj <mbmegharaj@gmail.com>
2024-07-29 15:45:23 +0530

    Complete implementation of task

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const mobileNavBtn = document.getElementById('mobile-nav-btn')
+const mobileNav = document.getElementById('mobile-nav')
+
+mobileNavBtn.addEventListener('click', (e) => {
+    if(mobileNav.style.height === '')
+    {
+        mobileNav.style.height = 'fit-content'
+    }else{
+        mobileNav.style.height = ''
+    }
+})
